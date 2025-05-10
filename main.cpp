2ae16410c9e165157884f75b3c9392e083cf901e
Tony Nguyen <tonixsmm>
2025-05-09 23:11:49 -0700

    fix typo

--- main.cpp
+++ main.cpp
@@ -17,9 +17,6 @@ int main() {
     river.addFeatureOnSpine("Lower Colombia", "Astoria", "City");
     river.addFeatureOnSpine("Astoria", "St. Helens", "City");
     river.addFeatureOnSpine("St. Helens", "Portland", "City");
-
-    Node* temp = river.findNode("Snake Trit")->parent;
-    std::cout << temp->name << std::endl;
     
     // Print the tree structure
     river.printTree();
